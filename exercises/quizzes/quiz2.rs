1b9a11154ab23ab4254c3384300f548925fa028f
John Waters <jp.waters@hotmail.com>
2026-02-27 18:30:54 +1100

    Did quiz 2. should take a break

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,28 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> { 
+        
+        let mut output = Vec::<String>::new();
+        for tuple in input {
+            let (mut string, command) = tuple;
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()), 
+                Command::Append(size) => {
+                    let mut bar = String::new();
+                    let mut counter = size;
+                    while counter > 0 {
+                        bar.push_str("bar");
+                        counter -= 1;
+                    }
+                    string.push_str(&bar);
+                    output.push(string);
+                }
+            }
+        }
+        output
+    }
 }
 
 fn main() {
@@ -37,7 +58,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
